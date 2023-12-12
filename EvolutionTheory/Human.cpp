9f4d3a521309e0c61e180ecae242d70b1b627dbc
cangu <cangu@DESKTOP-8430MDH>
2023-12-13 00:11:00 +0300

    Proje dosyası ekle.

---
+++ EvolutionTheory/Human.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+using namespace std;
+class Human
+{
+	//defining human abilities - perks || illnesses
+	int eyes_count;
+	int eyebrows_count;
+	int eyelashes_count;
+	int hand_count;
+	int leg_count;
+	int weight;
+	int height;
+	int age;
+	int shoulder_size;
+	bool isMentally_disordered;
+	bool isPhysically_disordered;
+	bool isInheritable;
+	string mental_disorderName;
+	string physical_disorderName;
+	string name;
+	string nationality;
+
+
+
+	Human()
+	{
+		eyes_count = 0;
+		eyebrows_count = 0;
+		eyelashes_count = 0;
+		hand_count = 0;
+		leg_count = 0;
+		weight = 0;
+		height = 0;
+
+
+	}
+
+};
